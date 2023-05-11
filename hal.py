1deb6411c283aed7de3d5e30939ef4df4bb7026e
david <david@DiscoMonster>
2023-05-10 23:40:29 -0500

    Added gpl3 license.

--- hal.py
+++ hal.py
@@ -2,7 +2,21 @@
 
 ####  Copyright 2023 David Caldwell disco47dave@gmail.com
 
-# openai.api_key = os.environ['OPENAI_API_KEY']
+
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+# 
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+# 
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 
 import os, re 
 from langchain.memory.vectorstore import VectorStoreRetrieverMemory
